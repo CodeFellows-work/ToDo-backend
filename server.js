e747294c15bfb7cd559ea8454b10420c44773a04
sunny-lee3 <sunnylee3712@gmail.com>
2021-07-31 14:40:15 -0700

    server working on /todos routes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const base64 = require('base-64');
-const cors = require('cors');
+
 
 
 
@@ -13,31 +13,36 @@ const { todos, users, db } = require('./lib/models/index.js');
 const app = express();
 
 app.use(express.json());
-app.use(cors);
 
 
-app.get('/todos', async (req, res) => {
+
+app.get('/:todos', async (req, res) => {
   let rows = await todos.findAll();
   res.json(rows);
 });
 
-app.post('/todos', (req, res) => {
+
+app.post('/todos', handleCreate);
+
+app.put('/:todos/:id', (req, res) => {
 
 });
 
-app.put('/todos/:id', (req, res) => {
+app.delete('/:todos/:id', (req, res) => {
 
 });
 
-app.delete('./todos/:id', (req, res) => {
-
-});
-
+async function handleCreate(req, res) {
+    let obj = req.body;
+    let records  = await req.todos.create(obj);
+    res.status(200).json(records);
+}
 
 app.post('/signup', async (req, res) => {
     let {username, password} = req.body;
     console.log('console.log from /signup post');
-    let user = await users.create({username, password});
+
+    let user =  await users.create({username, password});
     res.json({user, token: user.token}); 
 });
 
@@ -52,6 +57,11 @@ app.post('/signin', async (req, res) => {
     });
 });
 
+app.get('/users', async (req, res) => {
+    const userRecords = await users.findAll({});
+    const list = userRecords.map(user => user.username);
+    res.status(200).json(list);
+})
 
 module.exports = {
     app: app,
